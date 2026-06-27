de6b6d3056343e5b1388a8c774d2ebbab11cb222
Gawali Krushna <gkrushna893@gmail.com>
2026-06-28 00:37:50 +0530

    Add files via upload

---
+++ Codes/IterableListSumationFunction.py
@@ -0,0 +1,18 @@
+def Sumation(Data):
+
+    Sum = 0
+
+    for no in Data:
+        Sum = Sum + no
+
+    return Sum
+
+def main():
+    Marks = [78,90,56,98,77]
+
+    Ret = Sumation(Marks)
+
+    print("Addition is : ",Ret)
+
+if __name__ == "__main__":
+    main()
---
+++ Codes/LocalGlobal1.py
@@ -0,0 +1,11 @@
+no = 11  # Global variable
+
+def Display():
+    a = 21
+    print("From display : ",no)
+
+def Demo():
+    print("From demo : ",no)
+
+Display()
+Demo()
